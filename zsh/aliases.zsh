3cc61b650f8c6cafa21d074205b13ef9f46f73b8
Sabareesh Natarajan <sab.natarajan@gmail.com>
2020-07-20 01:00:40 -0400

    feat(zsh): update aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,10 +1,49 @@
+# DIRECTORY
 alias l="ls -lh"
 alias la="ls -lAh"
 
-alias g=git
+alias ..="cd .."
+alias ...="cd ../.."
+alias ....="cd ../../.."
+alias .....="cd ../../../.."
+alias ~="cd ~"
+alias 1="cd -"
+alias 2="cd -2"
+alias 3="cd -3"
+alias 4="cd -4"
+alias 5="cd -5"
+alias 6="cd -6"
+alias 7="cd -7"
+alias 8="cd -8"
+alias 9="cd -9"
+
+alias md="mkdir -p"
+alias rmr="rm -r"
+alias rmrf="rm -rf"
+
+# VIM
 alias vi=nvim
 alias vim=nvim
+
+# GIT
+alias g=git
+alias ga="git add"
+alias gc="git commit"
+alias gst="git status"
+alias gc="git commit"
+alias gcm="git commit -m"
+alias gl="git log"
+alias glo="git log --oneline --decorate"
+alias glog="git log --oneline --decorate --graph"
+alias glol="git log --graph --pretty=\"%Cred%h%Creset -%C(auto)%d%Creset %s%Cgreen (%cr) %C(bold blue)<%an>%Creset\""
+alias glola="git log --graph --pretty=\"%Cred%h%Creset -%C(auto)%d%Creset %s%Cgreen (%cr) %C(bold blue)<%an>%Creset\" --all"
+alias gf="git fetch"
+alias gp="git push"
+alias gl="git pull"
+alias gm="git merge"
+
 
 alias cl=clear
 alias k=kubectl
 alias dc=docker-compose
+

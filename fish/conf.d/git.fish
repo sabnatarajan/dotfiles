b3a892f06ac81c571eca57d26cd6fe95c23e9575
Sabareesh Natarajan <sab.natarajan@gmail.com>
2020-12-28 19:34:34 -0600

    update(fish): More git abbreviations

--- fish/conf.d/git.fish
+++ fish/conf.d/git.fish
@@ -12,29 +12,31 @@ abbr gco  "git checkout"
 abbr gcob "git checkout -b"
 abbr gclo "git clone"
 abbr gr   "git rev-parse --show-toplevel"
+abbr gre  "git restore"
+abbr grs  "git reset"
 
-# GIT: Commits
+# Commits
 abbr gc    "git commit -v"
 abbr gcm   "git commit -m"
 abbr gca   "git commit -v -a"
 abbr gcam  "git commit -v -a -m"
-abbr gc!   "git commit -v --amend"
-abbr gca!  "git commit -v -a --amend"
-abbr gcan! "git commit -v -a --amend --no-edit"
+abbr gcx   "git commit -v --amend"
+abbr gcxa  "git commit -v -a --amend"
+abbr gcxn  "git commit -v --amend --no-edit"
 
-# GIT: Log
+# Log
 abbr gl    "git log"
 abbr glo   "git log --oneline --decorate"
 abbr glog  "git log --oneline --decorate --graph"
 abbr glol  "git log --graph --pretty=\"%Cred%h%Creset -%C(auto)%d%Creset %s%Cgreen (%cr) %C(bold blue)<%an>%Creset\""
 abbr glola "git log --graph --pretty=\"%Cred%h%Creset -%C(auto)%d%Creset %s%Cgreen (%cr) %C(bold blue)<%an>%Creset\" --all"
 
-# GIT: Diff
+# Diff
 abbr gd   "git diff"
 abbr gds  "git diff --staged"
 abbr gdc  "git diff --cached"
 
-# GIT: Stash
+# Stash
 abbr gst  "git stash"
 abbr gstl "git stash list"
 abbr gstp "git stash pop"

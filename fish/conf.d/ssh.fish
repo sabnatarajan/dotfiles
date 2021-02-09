54aa3c7879115d3a14ece2783986a2ad4c1c4337
Sabareesh Natarajan <sab.natarajan@gmail.com>
2021-02-09 09:24:01 -0600

    feat(fish): Replace ssh-agent with gpg-agent for SSH

--- fish/conf.d/ssh.fish
+++
@@ -1,8 +0,0 @@
-# Autostart the SSH agent if not started
-set -l ssh_agent_pid (pgrep ssh-agent)
-if test "x" = "x$ssh_agent_pid"
-  eval (ssh-agent -c) > /dev/null
-  set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
-  set -Ux SSH_AGENT_PID $SSH_AGENT_PID
-  set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
-end

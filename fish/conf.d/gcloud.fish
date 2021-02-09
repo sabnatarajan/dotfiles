6ae0f7d10f60c1df0af60470be92981b135f9744
Sabareesh Natarajan <sab.natarajan@gmail.com>
2021-02-09 10:11:15 -0600

    update(fish): Clean up fish directories

--- fish/conf.d/gcloud.fish
+++
@@ -1,8 +0,0 @@
-set -Ux GCLOUD_HOME /usr/local/opt/google-cloud-sdk
-if test ! -d $GCLOUD_HOME
-    echo ERROR: google-cloud-sdk is not installed!
-    echo Install it using
-    echo "curl https://sdk.cloud.google.com | bash -s -- --install-dir=/usr/local/opt --disable-prompts"
-    exit 0
-end
-source $GCLOUD_HOME/path.fish.inc

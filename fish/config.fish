43f2d7765abf2bd1ea9bd66e04e04fcf96d594ee
Sabareesh Natarajan <sab.natarajan@gmail.com>
2021-05-31 18:21:54 -0400

    update(fish): test if asdf is installed before sourcing

--- fish/config.fish
+++ fish/config.fish
@@ -22,7 +22,7 @@ end
 # pyenv (asdf still uses pyenv under the hood)
 set -gx PYTHON_BUILD_ARIA2_OPTS "-x 10 -k 1M" # Use aria2c when downloading
 # asdf
-test (which asdf); and source $HOME/.asdf/asdf.fish
+type -f asdf; and source $HOME/.asdf/asdf.fish
 
 # Local config
 # if test -f ~/.local/fish/config.fish

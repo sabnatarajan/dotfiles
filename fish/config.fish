9d15e84863bb88c52af685592acc07780c70e7b4
Sabareesh Natarajan <sab.natarajan@gmail.com>
2021-06-25 10:45:10 -0400

    feat(fish): auto start X upon login

--- fish/config.fish
+++ fish/config.fish
@@ -7,12 +7,18 @@ set -U VISUAL nvim
 set -Ux STARSHIP_CONFIG ~/.config/starship/starship.toml
 starship init fish | source
 
+if status is-interactive
+and test (tty) = "/dev/tty1"
+  startx ~/.config/X11/xinit/xinitrc
+end
+
 # Autostart tmux
 # Adapted from https://github.com/fish-shell/fish-shell/issues/4434#issuecomment-332626369
 # only run in interactive (not automated SSH for example)
 if status is-interactive
 # don't nest inside another tmux
 and not set -q TMUX
+and not test (tty) = "/dev/tty1"
   # Adapted from https://unix.stackexchange.com/a/176885/347104
   # Create session 'main' or attach to 'main' if already exists.
   tmux new-session -A -s main

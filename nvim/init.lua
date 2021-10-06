68d335bf2aff203009f10e367019a7e5f3e734b4
Sab Natarajan <sab.natarajan@gmail.com>
2021-10-06 13:21:06 -0400

    feat(nvim): Add nvim-cmp for autocompletion

--- nvim/init.lua
+++ nvim/init.lua
@@ -24,9 +24,13 @@ require('packer').startup {
 
     use 'neovim/nvim-lspconfig'         -- Common configs for LSP client
     use 'nvim-lua/lsp_extensions.nvim'  -- Extensions for built-in LSP
-    use 'nvim-lua/completion-nvim'      -- Autocomplete
     use 'nvim-treesitter/nvim-treesitter'
-    
+
+    use 'hrsh7th/nvim-cmp' -- Autocompletion 
+    use 'hrsh7th/cmp-nvim-lsp' -- LSP source for nvim-cmp
+    use 'saadparwaiz1/cmp_luasnip' -- Snippets source for nvim-cmp
+    use 'L3MON4D3/LuaSnip' -- Snippets plugin
+
     -- Telescope (Fuzzy finder, file browser)
     use {
       'nvim-telescope/telescope.nvim', 
@@ -198,11 +202,59 @@ local servers = { 'pyright', 'rust_analyzer', 'tsserver', 'gopls', 'bashls' }
 for _, server in ipairs(servers) do
   lsp[server].setup {
     on_attach = on_attach,
+    capabilities = capabilities,
     flags = {
       debounce_text_changes = 150,
     }
   }
 end
+
+-- luasnip setup
+local luasnip = require 'luasnip'
+
+-- nvim-cmp setup
+local cmp = require 'cmp'
+cmp.setup {
+  snippet = {
+    expand = function(args)
+      require('luasnip').lsp_expand(args.body)
+    end,
+  },
+  mapping = {
+    ['<C-p>'] = cmp.mapping.select_prev_item(),
+    ['<C-n>'] = cmp.mapping.select_next_item(),
+    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.close(),
+    ['<CR>'] = cmp.mapping.confirm {
+      behavior = cmp.ConfirmBehavior.Replace,
+      select = true,
+    },
+    ['<Tab>'] = function(fallback)
+      if vim.fn.pumvisible() == 1 then
+        vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<C-n>', true, true, true), 'n')
+      elseif luasnip.expand_or_jumpable() then
+        vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<Plug>luasnip-expand-or-jump', true, true, true), '')
+      else
+        fallback()
+      end
+    end,
+    ['<S-Tab>'] = function(fallback)
+      if vim.fn.pumvisible() == 1 then
+        vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<C-p>', true, true, true), 'n')
+      elseif luasnip.jumpable(-1) then
+        vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<Plug>luasnip-jump-prev', true, true, true), '')
+      else
+        fallback()
+      end
+    end,
+  },
+  sources = {
+    { name = 'nvim_lsp' },
+    { name = 'luasnip' },
+  },
+}
 
 ---------------
 -- Treesitter

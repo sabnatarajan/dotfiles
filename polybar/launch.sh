c53eac6af9e3d286732335669080585e2b2df3fb
Sab Natarajan <sab.natarajan@gmail.com>
2021-10-19 11:29:20 -0400

    fix(polybar): Autostart correctly

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -6,15 +6,17 @@ else
   BAR=$1
 fi
 
-# Terminate already running bar instances
-killall -q polybar
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+polybar_pid() {
+  pgrep -l polybar | awk '{ print $1 }'
+}
 
-if type "xrandr"; then
-  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload $BAR &
-  done
-else
-  polybar --reload $BAR &
+if [[ -z $polybar_pid ]]; then
+  if type "xrandr"; then
+    for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+      MONITOR=$m polybar --reload $BAR &
+    done
+  else
+    polybar --reload $BAR &
+  fi
 fi
 

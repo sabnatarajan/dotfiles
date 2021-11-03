bc99744ede3f2418bc2ba4d90329d14dcbb6415b
Sab Natarajan <sab.natarajan@gmail.com>
2021-11-02 22:33:34 -0400

    fix: Make SSH, GPG and pinentry less cumbersome to use

--- fish/conf.d/gnupg.fish
+++ fish/conf.d/gnupg.fish
@@ -1,6 +1,6 @@
 # Ensure that GPG Agent is used as the SSH agent
 set -e SSH_AUTH_SOCK
-set -U -x SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
+set -Ux SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
 
 set -x GPG_TTY (tty)
 

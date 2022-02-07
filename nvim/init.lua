357831b832de9f42865877f2dac39696a5abe40b
Sabareesh Natarajan <sab.natarajan@gmail.com>
2022-02-06 20:42:57 -0500

    feat(nvim): Add toggleterm

--- nvim/init.lua
+++ nvim/init.lua
@@ -43,3 +43,58 @@ ts.setup {ensure_installed = 'maintained', highlight = {enable = true}}
 --  Glow (Markdown)
 ----------------------
 utils.keymap('n', '<leader>md', ':Glow<CR>')
+
+
+-----------------------------------------------------------------------
+-- Toggleterm config
+-----------------------------------------------------------------------
+local toggleterm = require "toggleterm"
+local term = require "toggleterm.terminal".Terminal
+
+function term_on_open(term)
+  vim.api.nvim_buf_set_keymap(term.bufnr, 'n', 'qq', "<cmd>close<CR>", {noremap=true, silent=true})
+end
+
+toggleterm.setup({
+  size = 20,
+  open_mapping = [[<leader>tt]],
+  hide_numbers = true,
+  start_in_insert = true,
+  shell = "fish",
+  direction = "float",
+  float_opts = { 
+    border = "curved", 
+    winblend = 0, 
+    highlights = { border = "Normal", background = "Normal", },
+  },
+  on_open = term_on_open
+})
+
+-- helpful mappings to make moving in and out of a terminal easier once toggled, 
+-- whilst still keeping it open.
+function _G.set_terminal_keymaps()
+  local opts = {noremap = true}
+  vim.api.nvim_buf_set_keymap(0, 't', '<esc>', [[<C-\><C-n>]], opts)
+  vim.api.nvim_buf_set_keymap(0, 't', '<C-h>', [[<C-\><C-n><C-W>h]], opts)
+  vim.api.nvim_buf_set_keymap(0, 't', '<C-j>', [[<C-\><C-n><C-W>j]], opts)
+  vim.api.nvim_buf_set_keymap(0, 't', '<C-k>', [[<C-\><C-n><C-W>k]], opts)
+  vim.api.nvim_buf_set_keymap(0, 't', '<C-l>', [[<C-\><C-n><C-W>l]], opts)
+  vim.api.nvim_buf_set_keymap(0, 't', '<C-q>', "<cmd>ToggleTerm<CR>", opts)
+end
+
+-- if you only want these mappings for toggle term use term://*toggleterm#* instead
+vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
+
+-- custom commands
+local htop = term:new({ cmd = 'htop', hidden = true, on_open = term_on_open })
+function htop_toggle() htop:toggle() end
+utils.keymap('n', '<leader>tht', '<cmd>lua htop_toggle()<CR>')
+
+local lazygit = term:new({ cmd = 'lazygit', hidden = true, on_open = term_on_open })
+function lazygit_toggle() lazygit:toggle() end
+utils.keymap('n', '<leader>tlg', '<cmd>lua lazygit_toggle()<CR>')
+
+local python = term:new({ cmd = 'python3', hidden = true, on_open = term_on_open })
+function python_toggle() python:toggle() end
+utils.keymap('n', '<leader>tpy', '<cmd>lua python_toggle()<CR>')
+
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -82,6 +82,8 @@ return packer.startup(function(use)
 
   use 'lewis6991/gitsigns.nvim'
 
+  use 'akinsho/toggleterm.nvim'
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if packer_bootstrap then

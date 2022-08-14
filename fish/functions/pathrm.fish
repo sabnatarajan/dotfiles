dac8136462b0cd635c3631521d204a055400cd5a
Sab Natarajan <sab.natarajan@gmail.com>
2022-08-14 09:29:23 -0400

    fish: update pathrm

--- fish/functions/pathrm.fish
+++ fish/functions/pathrm.fish
@@ -2,6 +2,11 @@ function pathrm -d "Remove a path from fish_user_paths"
   if test (count $argv) = 0
     echo "Usage: pathrm <index> (Use pathls to get index)" 
   else
-    set --erase --universal fish_user_paths[$argv[1]]
+    set -l p $fish_user_paths[$argv[1]]
+    read -l -P "Delete $p? [y/N]: " confirm
+    switch $confirm
+      case Y y
+        set --erase --universal fish_user_paths[$argv[1]]
+    end
   end
 end

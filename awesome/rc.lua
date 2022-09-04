a6c38fd55daa0121186267d7f6a4f3148a585431
Sab Natarajan <sab.natarajan@gmail.com>
2022-09-04 12:55:56 -0400

    awesome: Make tiling the default layout on all tags

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -122,7 +122,7 @@ awful.screen.connect_for_each_screen(function(s)
   set_wallpaper(s)
 
   -- Each screen has its own tag table.
-  awful.tag({ "1", "2", "3", "4", "5", "6", "7", "8", "9" }, s, layouts)
+  awful.tag({ "1", "2", "3", "4", "5", "6", "7", "8", "9" }, s, layouts[1])
 
   -- Create a promptbox for each screen
   s.widget_prompt = awful.widget.prompt()

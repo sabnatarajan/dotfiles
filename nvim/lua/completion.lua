ce8ce463ab613c1faaf368ce171a300d17179f2d
Sab Natarajan <sab.natarajan@gmail.com>
2023-04-09 10:28:50 -0400

    nvim: update

--- nvim/lua/completion.lua
+++ nvim/lua/completion.lua
@@ -28,7 +28,7 @@ cmp.setup {
 
 -- Setup lspconfig.
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
 return {
   capabilities = capabilities

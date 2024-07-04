01d0c12796be0ec45ba1dc3160653ac0b54f0fa8
Sab Natarajan <sab.natarajan@gmail.com>
2024-07-03 21:06:51 -0400

    remove unused config

--- fish/config.fish
+++ fish/config.fish
@@ -16,24 +16,5 @@ if status is-interactive
   source ~/.config/fish/setup.fish
 end
 
-# Autostart tmux
-# Adapted from https://github.com/fish-shell/fish-shell/issues/4434#issuecomment-332626369
-# only run in interactive (not automated SSH for example)
-if status is-interactive
-# don't nest inside another tmux
-and not set -q TMUX
-and not test (tty) = "/dev/tty1"
-  # Adapted from https://unix.stackexchange.com/a/176885/347104
-  # Create session 'main' or attach to 'main' if already exists.
-  tmux new-session -A -s main
-  gpg-connect-agent updatestartuptty /bye > /dev/null;
-end
-
-## ASDF Config
-# pyenv (asdf still uses pyenv under the hood)
-set -gx PYTHON_BUILD_ARIA2_OPTS "-x 10 -k 1M" # Use aria2c when downloading
-# asdf
-type -f asdf > /dev/null 2>&1; and source $HOME/.asdf/asdf.fish
-
 # Local config
 test -f ~/.local/fish/config.fish; and source ~/.local/fish/config.fish

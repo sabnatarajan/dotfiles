0e1535dd3fe8bce90b049117e0b7e5b28307559e
Sab Natarajan <sab.natarajan@gmail.com>
2024-07-03 21:25:43 -0400

    wezterm update

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -42,11 +42,12 @@ config.keys = {
 	{ key = "i", mods = "LEADER", action = act.SplitHorizontal { domain = "CurrentPaneDomain" } },
 	{ key = "o", mods = "LEADER", action = act.SplitVertical { domain = "CurrentPaneDomain" } },
 
+	{ key = "h", mods = "LEADER", action = act.ActivatePaneDirection("Left") },
 	{ key = "j", mods = "LEADER", action = act.ActivatePaneDirection("Down") },
 	{ key = "k", mods = "LEADER", action = act.ActivatePaneDirection("Up") },
 	{ key = "l", mods = "LEADER", action = act.ActivatePaneDirection("Right") },
-	{ key = "x", mods = "LEADER", action = act.CloseCurrentPane { confirm = true } },
 
+	{ key = "x", mods = "LEADER", action = act.CloseCurrentPane { confirm = true } },
 	{ key = "z", mods = "LEADER", action = act.TogglePaneZoomState },
 	{ key = "s", mods = "LEADER", action = act.RotatePanes "Clockwise" },
 
